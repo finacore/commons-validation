6dfa8d78015653a423bb08f0fd0555ff9bab40de
Denys G. Santos <gsdenys@gmail.com>
2022-09-10 15:20:28 -0400

    document translator code

--- translator.go
+++ translator.go
@@ -1,3 +1,10 @@
+// commonserrors package was created with intent to standardizing a way to execute the data
+// structure validation processo that returns the validation erros every in the same way and
+// in the same language.
+//
+// by convention, the english language was choosen as a default and unique language. Case your
+// code want to use a diferente language the better for you is do not use this library and and
+// choose to use the standard form of the go-playground/validator library
 package commonsvalidation
 
 import (
