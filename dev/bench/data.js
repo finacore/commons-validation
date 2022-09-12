89ffef12b09f7f43fb12b185911232911f717231
github-action-benchmark <github@users.noreply.github.com>
2022-09-12 19:52:51 +0000

    add Benchmark (go) benchmark result for 50e44fc086b38e1f65082ed4493c4ad5b47babb3

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1663012371577,
+  "repoUrl": "https://github.com/finacore/commons-validation",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "gsdenys@gmail.com",
+            "name": "Denys G. santos",
+            "username": "gsdenys"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "50e44fc086b38e1f65082ed4493c4ad5b47babb3",
+          "message": "Merge pull request #4 from finacore/pipeline\n\ncreate benchmark for model and new",
+          "timestamp": "2022-09-12T15:52:02-04:00",
+          "tree_id": "bbf9e6f4b25f3f34de0c94a6b786138d140e39d5",
+          "url": "https://github.com/finacore/commons-validation/commit/50e44fc086b38e1f65082ed4493c4ad5b47babb3"
+        },
+        "date": 1663012369877,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "Benchmark_New",
+            "value": 0.0001461,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "Benchmark_Validate_Model",
+            "value": 0.0001839,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
